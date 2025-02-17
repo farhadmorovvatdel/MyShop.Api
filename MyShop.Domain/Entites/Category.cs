d84a3acd6377d6b9c4225250ed915b293a09e066
farhad morovvatdel <f.morovvatdel@gmail.com>
2025-02-17 17:34:44 +0330

    setup project

---
+++ MyShop.Domain/Entites/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Domain.Entites
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength (50)]
+        
+        public string? Description { get; set; }
+    }
+}
