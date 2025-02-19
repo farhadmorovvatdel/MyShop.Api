3c1ae727e39dbb16fda089d42e2d6c26fca65aab
farhad morovvatdel <f.morovvatdel@gmail.com>
2025-02-19 14:06:14 +0330

    addEntityProduct

---
+++ MyShop.Domain/Entites/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Domain.Entites
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(150)]
+        
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string? ProductImage { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+
+
+        #region realtion
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        #endregion
+
+
+    }
+}
