5064a92da3605625abba0db4ca67f4b2616c3f74
farhad morovvatdel <f.morovvatdel@gmail.com>
2025-02-19 14:05:23 +0330

    createmapforProduct

---
+++ MyShop.Application/Mapping/ProductProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MyShop.Application.Dto.Product;
+using MyShop.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Application.Mapping
+{
+    public class ProductProfile:Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>();
+        }
+    }
+}
