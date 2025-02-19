fe8ddac1d5b62a63ab623f68bff1065397f4062c
farhad morovvatdel <f.morovvatdel@gmail.com>
2025-02-19 14:04:16 +0330

    addProductDto

---
+++ MyShop.Application/Dto/Product/ProductDto.cs
@@ -0,0 +1,29 @@
+﻿using MyShop.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Application.Dto.Product
+{
+    public class ProductDto
+    {
+       
+       
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public decimal Price { get; set; }
+
+        public string? ProductImage { get; set; }
+        public DateTime Created { get; set; } 
+
+
+        
+        
+    }
+}
