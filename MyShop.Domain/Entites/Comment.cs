e5987a887f361fb42c23298ed0b9b573274c8f8f
farhad morovvatdel <f.morovvatdel@gmail.com>
2025-02-22 13:39:31 +0330

    addCommnetEntity

---
+++ MyShop.Domain/Entites/Comment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Domain.Entites
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]    
+        public string CommentBody { get; set; } 
+
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        #region #relations
+        [ForeignKey(nameof(UserId))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; }
+
+        public int ProductId { get; set; }
+        #endregion
+    }
+}
